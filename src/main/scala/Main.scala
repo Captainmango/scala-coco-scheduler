06c5961acd5e2c638e189e2c0c0332c9abfa3369
Edward Heaver <edward.heaver01@gmail.com>
2026-08-31 19:07:50 +0100

    Handle error case on left branch

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,8 +4,8 @@ import domain.parser.Lexer
 @main
 def hello(): Unit =
   val parser = new Parser(Lexer.make(""))
-  val result = parser.parse("99 * * * *") match {
-    case Left(value) => ???
+  val result = parser.parse("99 * * *") match {
+    case Left(value) => s"This errored: ${value}"
     case Right(value) => value
   }
 
