ae8d878edabf01738f83bfac01c033216899b4a1
Edward Heaver <edward.heaver01@gmail.com>
2026-09-03 13:54:39 +0100

    format

--- src/main/scala/domain/parser/Errors.scala
+++ src/main/scala/domain/parser/Errors.scala
@@ -1,15 +1,15 @@
 package domain.parser
 
-sealed trait ParserError
+sealed trait ParserError { def message: String }
 case class InvalidInput(raw: String, position: Int) extends ParserError {
-  override def toString(): String = s"Invalid input at position $position. Near $raw"
+  def message: String = s"Invalid input at position $position. Near $raw"
 }
 case class ExpectedNumber(raw: String, position: Int) extends ParserError {
-  override def toString(): String = s"Expected number at position $position. Near $raw"
+  def message: String = s"Expected number at position $position. Near $raw"
 }
 case class CronInvalid(rawExpr: String) extends ParserError {
-  override def toString(): String = s"Provided cron expression $rawExpr is not a valid cron."
+  def message: String = s"Provided cron expression $rawExpr is not a valid cron."
 }
 case class OutOfIntervalBounds(ci: CronInterval, v: String) extends ParserError {
-  override def toString(): String = s"$v outside range of ${ci.lowerBound} to ${ci.upperBound}"
+  def message: String = s"$v outside range of ${ci.lowerBound} to ${ci.upperBound}"
 }
